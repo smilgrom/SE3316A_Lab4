575cc539e761e3d613bbd8bffad678e0fd82c05e
Sydney Milgrom <s.milgrom@hotmail.com>
2015-11-26 14:27:24 -0500

    first commit

---
+++ public/app.js
@@ -0,0 +1,21 @@
+ 
+ var Mean = angular.module('Mean', ['ngRoute']);
+ 
+ url = "http://localhost:1330"
+
+ // configure our routes
+ Mean.config(function($routeProvider) {
+
+     $routeProvider
+     .when('/', {
+         templateUrl: 'views/login.html',
+         controller: 'loginController'
+     })
+    .otherwise({
+        redirectTo: '/'
+      });
+ });
+ 
+ Mean.controller('mainController', function($scope) {
+ 	
+ });
---
+++ routes/routes.js
@@ -0,0 +1,10 @@
+
+
+//ROUTES ===========================================================
+
+module.exports = function (app){
+
+    app.get('/login', function(req, res) {
+        console.log('Loggin it')
+    });
+}
